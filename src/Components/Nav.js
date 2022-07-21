341da0f0c184ede4fb103ed030df3ed176bdd7ec
Mrinal <mrinalsingh367@gmail.com>
2022-07-21 14:42:34 +0530

    navbar
    navbar updated

---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+import './Nav.css'
+
+const Nav = () => {
+    return(
+        <div className="main-container">
+            <h2>YourChallenge</h2>
+        <nav className="navbar-container">
+            <ul className="navbar-ist-container"> 
+                <li className='navbar-list'>Product</li>
+                <li className='navbar-list'>Downloading</li>
+                <li className='navbar-list'>Pricing</li>
+            </ul>
+        </nav>
+        </div>
+    )
+}
+
+export default Nav
